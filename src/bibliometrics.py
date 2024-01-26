35c4a87bb211323dd875f11c3794406a01c6245c
Florian Ulrich Jehn <florianjehn@posteo.de>
2024-01-26 14:06:00 +0100

    Trying to fix the inf bug

--- src/bibliometrics.py
+++ src/bibliometrics.py
@@ -1,14 +1,13 @@
 import numpy as np
-import sklearn
 import pandas as pd
 import networkx as nx
+from sklearn.metrics.pairwise import cosine_similarity
+from factor_analyzer import FactorAnalyzer
 import os
 
-def prepare_matrix_from_relevant_pubs(path):
+def create_graph_from_dimensions_full_dataset(path):
     """
-    This creates a citation matrix from the relevant publications. 
-    This means those publications which actually have the topics
-    we are interested in in the abstract or title.
+    This creates a directed graph from the full dataset of publications.
 
     Arguments:
         path: path to the csv file containing the relevant publications
@@ -16,7 +15,7 @@ def prepare_matrix_from_relevant_pubs(path):
     Returns:
         A citation matrix
     """
-    raw_df = pd.read_csv(path, index_col=0, nrows=100)
+    raw_df = pd.read_csv(path, index_col=0, nrows=300)
     # set the index to the publication id
     raw_df.set_index("id", inplace=True)
 
@@ -45,37 +44,98 @@ def prepare_matrix_from_relevant_pubs(path):
             # Just add the node to the graph without any edges
             G.add_node(citing_paper)
 
+    return G
+
+
+def prepare_matrix_from_graph(G, citation_metric = "bib_coupling"):
+    """
+    This function takes a graph and creates a co-citation matrix from it.
+    And applies the cosine similarity to it to normalize it. This takes
+    into account the whole objects citation network at once. 
+
+    Arguments:
+        G: A directed graph
+        citation_metric: The citation metric that should be used. This
+        can be either "bib_coupling" or "co_citation"
+
+    Returns:
+        A co-citation matrix with cosine similarity applied
+    """
     # Create a matrix from the graph
     g_adj = nx.adjacency_matrix(G)
 
-    # Create co-citation matrix
-    cc_matrix = g_adj.transpose().dot(g_adj)
+    # Apply the citation metric
+    if citation_metric == "bib_coupling":
+        matrix = g_adj.dot(g_adj.transpose())
+    elif citation_metric == "co_citation":
+        matrix = g_adj.transpose().dot(g_adj)
+    else:
+        raise ValueError("Unknown citation metric")
 
     # Apply cosine similarity
-    cc_matrix = cosine_similarity(cc_matrix)
+    matrix = cosine_similarity(matrix)
 
-    # Create a dataframe from the matrix
-    cc_df = pd.DataFrame(cc_matrix, index=raw_df.index, columns=raw_df.index)
+    return matrix
 
-    # Save the dataframe to a csv file
-    cc_df.to_csv(
-        "data"
-        + os.sep
-        + "dimensions_test_dataset"
-        + os.sep
-        + "citation_matrix.csv"
-    )
+
+def factor_analysis(cc_matrix):
+    """
+    This function takes a co-citation matrix and applies factor analysis
+    to it. This is used to reduce the dimensionality of the matrix.
+
+    Arguments:
+        cc_matrix: A co-citation matrix
+
+    Returns:
+        A reduced co-citation matrix
+    """
+    fa1 = FactorAnalyzer(n_factors=len(cc_matrix), rotation=None)
+
+    # Make sure that the matrix is does not contain any nans or infs
+    assert np.count_nonzero(np.isnan(cc_matrix)) == 0
+    assert np.count_nonzero(np.isinf(cc_matrix)) == 0
+
+    fa1.fit(cc_matrix)
+    ev, v = fa1.get_eigenvalues()
+
+    # Get the number of factors that explain 90% of the variance
+    n_factors = 0
+    for i in range(len(ev)):
+        if ev[i] > 1:
+            n_factors += 1
+
+    # Apply factor analysis
+    # Using promax rotation, as the factors are expected to be correlated
+    fa2 = FactorAnalyzer(n_factors=n_factors, rotation="promax")
+    fa2.fit(cc_matrix)
+    ev, v = fa2.get_eigenvalues()
+
+    # Get the factor loadings
+    loadings = fa2.loadings_
+
+    # Get the factor scores
+    factor_scores = fa2.transform(cc_matrix)
+
+    return ev, loadings, factor_scores
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    matrix = prepare_matrix_from_relevant_pubs(
+    graph = create_graph_from_dimensions_full_dataset(
         "data"
         + os.sep
         + "dimensions_test_dataset"
         + os.sep
         + "publications_with_CT_in_titles_abstracts.csv"
     )
+    cc_matrix = prepare_matrix_from_graph(graph)
+
+    ev, loadings, factor_scores = factor_analysis(cc_matrix)
+    print(ev)
+    print(loadings)
+    print(factor_scores)
     
