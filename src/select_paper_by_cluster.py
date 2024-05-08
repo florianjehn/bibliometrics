6f3a33b52d7c1b5df1703d3b2b90d636702e9c29
Florian Ulrich Jehn <florianjehn@posteo.de>
2024-05-08 12:11:56 +0200

    Added paper lists

--- src/select_paper_by_cluster.py
+++ src/select_paper_by_cluster.py
@@ -10,3 +10,19 @@ assert clustered_papers["Cluster"].nunique() == 9, f"Number of clusters is not 9
 }"
 assert clustered_papers["Cluster"].isnull().sum() == 0, "There are NaN values in the Cluster column"
 
+clusters = clustered_papers.groupby("Cluster")
+
+# Go over the clusters and select the 10 papers with the highest normalized citations 
+# and total link strength
+# Write those to a csv seperate for each cluster
+for cluster_name, cluster in clusters:
+    sorted_strength = cluster.sort_values(by=["Total link strength"], ascending=False)
+    main_strength = sorted_strength.head(15).copy()
+    main_strength.loc[:, "Selected by"] = "Total link strength"
+    sorted_citations = cluster.sort_values(by=["Norm. citations"], ascending=False)
+    main_citations = sorted_citations.head(15).copy()
+    main_citations.loc[:, "Selected by"] = "Norm. citations"
+    main_cluster = pd.concat([main_strength, main_citations])
+    main_cluster.to_csv(
+        f"data{os.sep}main_papers{os.sep}cluster_{cluster_name}_main_works.csv", index=False
+    )
