aef8f430ea1d44422ea8731314ac5cb1360a8622
Florian Ulrich Jehn <florianjehn@posteo.de>
2024-08-29 09:16:20 +0200

    Finished the meta data plots

--- src/select_paper_by_cluster_and_overall_relevance.py
+++ src/select_paper_by_cluster_and_overall_relevance.py
@@ -47,17 +47,22 @@ for cluster_name, cluster in clusters:
     )
 
 # Do the same for the overall dataset
-# and would only clutter the data
+# But first remove the discarded clusters
+other_topic_clusters = [6, 7, 8, 14, 15, 16, 18, 19, 22, 23]
+clustered_papers = clustered_papers[~clustered_papers["Cluster"].isin(other_topic_clusters)]
 num_papers = 15
 sorted_strength = clustered_papers.sort_values(by=["Total link strength"], ascending=False)
 main_strength = sorted_strength.head(num_papers).copy()
+sorted_relevance = clustered_papers.sort_values(by=["relevance_score"], ascending=False)
+main_relevance = sorted_relevance.head(num_papers).copy()
+main_relevance.loc[:, "Selected by"] = "Relevance"
 main_strength.loc[:, "Selected by"] = "Total link strength"
 sorted_citations = clustered_papers.sort_values(by=["Norm. citations"], ascending=False)
 main_citations_normed = sorted_citations.head(num_papers).copy()
 main_citations_normed.loc[:, "Selected by"] = "Norm. citations"
 main_citations = clustered_papers.sort_values(by=["Citations"], ascending=False).head(num_papers).copy()
 main_citations.loc[:, "Selected by"] = "Citations"
-main_works = pd.concat([main_strength, main_citations_normed, main_citations])
+main_works = pd.concat([main_relevance, main_strength, main_citations_normed, main_citations])
 # Put the selected by column third
 cols = main_works.columns.tolist()
 cols = cols[:3] + cols[-1:] + cols[3:-1]
