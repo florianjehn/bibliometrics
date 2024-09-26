fac2267f0dac86bec0c96f249a263e5a0fbf08de
Florian Ulrich Jehn <florianjehn@posteo.de>
2024-09-26 15:14:05 +0200

    Added the excluded papers, so they can be more easily scanned

--- src/remerge_data.py
+++ src/remerge_data.py
@@ -15,16 +15,23 @@ df = pd.merge(vos_meta, open_alex, left_on="Title", right_on="display_name", how
 df_duplicates = df[df.duplicated(subset="Title", keep=False)]
 df_duplicates.to_csv(f"data{os.sep}prepared{os.sep}remerge_duplicates.csv", index=False)
 
+
 # The df now contains a bunch of duplicates. Therefore, we go through the dataframes
 # and check for duplicates
 # When we find duplicates we keep the ones with the higest number of citations.
 shape_before = df.shape[0]
 df = df.sort_values(by=["cited_by_count"], ascending=False)
 df = df.drop_duplicates(subset="Title", keep="first")
+
 # This should remove some duplicates
 assert shape_before > df.shape[0]
 
-# Remove the clearly irrelevant papers with a total link strength smaller than 10
+# Create a dataframe that contains all the papers that are in the open_alex dataset
+# but not in df
+df_missing = open_alex[~open_alex["display_name"].isin(df["Title"])]
+df_missing.to_csv(f"data{os.sep}prepared{os.sep}remerge_missing.csv", index=False)
+
+# Remove the clearly irrelevant papers with a total link strength smaller than 2
 num_papers_before = df.shape[0]
 df = df[df["Total link strength"] > 1]
 num_papers_after = df.shape[0]
